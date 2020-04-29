18cf943b60f30395f45a75e0b20713cd2345703b
Agustin Velasco Torres <aguslb@icloud.com>
2020-04-28 23:17:25 -0500

    adding files

--- src/main/java/com/udemy/model/Person.java
+++ src/main/java/com/udemy/model/Person.java
@@ -1,5 +1,6 @@
 package com.udemy.model;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -15,7 +16,7 @@ public class Person {
 	private String name;
 	
 	@NotNull
-	@Size(min=18)
+	@Min(18)
 	private int age;
 
 	public Person(String name, int age) {
